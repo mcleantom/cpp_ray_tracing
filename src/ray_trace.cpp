6501b8ec1ca98fbb57cf8ff7ef70b471d9dcb8be
Tom McLean <mcleantom97@gmail.com>
2022-07-25 21:52:25 +0100

    skybox

--- src/ray_trace.cpp
+++ src/ray_trace.cpp
@@ -5,6 +5,11 @@
 #include <vector>
 #include "geometry.h"
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 struct Material {
     Material(const float &r, const Vec4f &a, const Vec3f &color, const float &spec) : refractive_index(r), albedo(a), diffuse_color(color), specular_exponent(spec) {}
     Material() : refractive_index(1), albedo(1,0,0,0), diffuse_color(), specular_exponent() {}
@@ -35,6 +40,31 @@ struct Sphere {
         if (t0 < 0) return false;
         return true;
     }
+};
+
+struct SkyBox {
+    SkyBox(char const *f) : filename(f), req_comp(0) {
+        unsigned char *pixmap = stbi_load(filename, &width, &height, &comp, req_comp);
+        if (!pixmap || 3!=comp) {
+            std::cerr << "Cannog load the skybox image." << std::endl;
+            std::abort();
+        }
+
+        envmap = std::vector<Vec3f>(width*height);
+        for (int j = height-1; j>=0; j--) {
+            for (int i = 0; i<width; i++) {
+                envmap[i+j*width] = Vec3f(pixmap[(i+j*width)*3 + 0], pixmap[(i+j*width)*3 + 1], pixmap[(i+j*width)*3 + 2])*(1/255.);
+            }
+        }
+
+        stbi_image_free(pixmap);
+    }
+    char const *filename;
+    std::vector<Vec3f> envmap;
+    int width;
+    int height;
+    int comp;
+    int req_comp;
 };
 
 struct Light {
@@ -85,7 +115,7 @@ bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphe
     return spheres_dist < 1000;
 }
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, size_t depth=0) {
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, const std::vector<Light> &lights, const SkyBox &skybox, size_t depth=0) {
     /*
         Phone reflection model
         Intensity_point = Ambiant + Sum(each light) { Diffusive + Reflective (specular) }
@@ -113,16 +143,20 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
           specular_light_intensity = 0;
 
     if (depth >= 4 || !scene_intersect(orig, dir, spheres, point, N, material)) {
-        return Vec3f(0.2, 0.7, 0.8);  // Background Colour
+        int x = (int) ((atan2(dir.z, dir.x) / (2 * M_PI) + 0.5) * skybox.width);
+        int y = (int) (acos(dir.y) / M_PI * skybox.height);
+        x = std::max(0, std::min(x, skybox.width-1));
+        y = std::max(0, std::min(y, skybox.height-1));
+        return skybox.envmap[x + y*skybox.width];
     }
 
     Vec3f reflect_dir = reflect(dir, N).normalize();
     Vec3f reflect_orig = reflect_dir*N < 0 ? point - N*1e-3 : point + N*1e-3;
-    Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, depth+1);
+    Vec3f reflect_color = cast_ray(reflect_orig, reflect_dir, spheres, lights, skybox, depth+1);
 
     Vec3f refract_dir = refract(dir, N, material.refractive_index).normalize();
     Vec3f refract_orig = refract_dir*N < 0 ? point - N*1e-3 : point + N*1e-3;
-    Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, depth+1);
+    Vec3f refract_color = cast_ray(refract_orig, refract_dir, spheres, lights, skybox, depth+1);
 
     for (size_t i=0; i<lights.size(); i++) {
         Vec3f light_dir = (lights[i].position - point).normalize();
@@ -149,9 +183,9 @@ Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &s
     return material.diffuse_color * diffuse_light_intensity * material.albedo[0] + Vec3f(1., 1., 1.)*specular_light_intensity*material.albedo[1] + reflect_color*material.albedo[2] + refract_color*material.albedo[3];  // Sphere colour
 }
 
-void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights) {
-    const int width    = 1024;
-    const int height   = 768;
+void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights, const SkyBox &skybox) {
+    const int width    = 1024*4;
+    const int height   = 768*4;
     const int fov      = M_PI/2.;
 
     std::vector<Vec3f> framebuffer(width*height); // One dimensional array of Vec3f values (r, g, b)
@@ -161,7 +195,7 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
             float x = (2*(i + 0.5)/(float)width - 1)*tan(fov/2.)*width/(float)height;
             float y = -(2*(j+0.5)/(float)height - 1)*tan(fov/2.);
             Vec3f dir = Vec3f(x, y, -1).normalize();
-            framebuffer[i+j*width] = cast_ray(Vec3f(0, 0, 0), dir, spheres, lights);
+            framebuffer[i+j*width] = cast_ray(Vec3f(0, 0, 0), dir, spheres, lights, skybox);
         }
     }
 
@@ -180,6 +214,10 @@ void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights
 }
 
 int main() {
+    // int envmap_width, envmap_height;
+    // int n = -1;
+    // unsigned char *pixmap = stbi_load("../envmap2.jpg", &envmap_width, &envmap_height, &n, 0);
+    SkyBox    coastline("../envmap.jpg");
     Material      ivory(1.0, Vec4f(0.6,  0.3, 0.1, 0.0), Vec3f(0.4, 0.4, 0.3),   50.);
     Material      glass(1.5, Vec4f( 0.,  0.5, 0.1, 0.8), Vec3f(0.6, 0.7, 0.8),  125.);
     Material red_rubber(1.0, Vec4f(0.9,  0.1, 0.0, 0.0), Vec3f(0.3, 0.1, 0.1),   10.);
@@ -197,6 +235,6 @@ int main() {
         Light(Vec3f( 30, 50, -25), 1.8),
         Light(Vec3f( 30, 20,  30), 1.)
     };
-    render(spheres, lights);
+    render(spheres, lights, coastline);
     return 0;
 }
